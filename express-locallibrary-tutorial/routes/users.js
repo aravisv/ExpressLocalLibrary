d4e97ceacad2a2dbb573f99545835f95ab3ba3ef
Aravi MAC2 <aravisv@gmail.com>
2024-12-19 09:44:40 +0530

    router setup

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a resource"); //sends whatever is here as the response
+  //res.render("index", { title: "Users page using index template" });
 });
+
+/* 
+router.get("/cool", (req, res, next) => {
+  res.send("you are cool - (from the same users router)");
+}); 
+*/
 
 module.exports = router;
