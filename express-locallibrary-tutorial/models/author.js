d27b8c9ca37e127c91fd3641e26e4496563c5350
Aravinda S V <aravindasv@Aravindas-Laptop.local>
2025-01-19 13:42:02 +0530

    created more static pages with links and data fetching

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
@@ -11,13 +11,10 @@ const AuthorSchema = new Schema({
 
 // Virtual for author's full name
 AuthorSchema.virtual("name").get(function () {
-  // To avoid errors in cases where an author does not have either a family name or first name
-  // We want to make sure we handle the exception by returning an empty string for that case
   let fullname = "";
   if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
   }
-
   return fullname;
 });
 
@@ -25,6 +22,22 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("dob_formatted").get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+AuthorSchema.virtual("dod_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+AuthorSchema.virtual("life_span").get(function () {
+  return `${this.dob_formatted} - ${this.dod_formatted}`;
 });
 
 // Export model
